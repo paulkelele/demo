5ecf27f94ac2fc4de6a7bd57cdf0ad9e1bb60404
stef boy <stef@exemple.com>
2022-08-23 16:21:09 +0200

    ajout friendchip

---
+++ src/main/java/root/FriendShip.java
@@ -0,0 +1,28 @@
+package root;
+
+public class FriendShip {
+    private int id_requester ;
+    private int id_requestee ;
+    private char type;
+    
+    public FriendShip(){}
+
+    public int getId_requester() {
+        return id_requester;
+    }
+    public void setId_requester(int id_requester) {
+        this.id_requester = id_requester;
+    }
+    public int getId_requestee() {
+        return id_requestee;
+    }
+    public void setId_requestee(int id_requestee) {
+        this.id_requestee = id_requestee;
+    }
+    public char getType() {
+        return type;
+    }
+    public void setType(char type) {
+        this.type = type;
+    }
+}
